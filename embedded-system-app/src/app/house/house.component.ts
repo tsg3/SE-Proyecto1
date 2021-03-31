6b39967c458f21160485ed6d4e5497491706ab3d
Esteban Campos Granados <estebandcg1999@gmail.com>
2021-03-31 12:46:26 -0600

    Frontend's light feature added

--- embedded-system-app/src/app/house/house.component.ts
+++ embedded-system-app/src/app/house/house.component.ts
@@ -8,6 +8,8 @@ import { TokenStorageService } from '@app/_services/token-storage.service';
 })
 export class HouseComponent implements OnInit {
   isLoggedIn = false;
+  // Sin backend
+  public lightsOn = [false, false, false, false, false];
 
   constructor(private token: TokenStorageService) { }
 
@@ -15,4 +17,32 @@ export class HouseComponent implements OnInit {
     this.isLoggedIn = !!this.token.getToken();
   }
 
+  // Sin backend
+  switchLight (light: number): void {
+    this.lightsOn[light] = !this.lightsOn[light];
+  }
+
+  // Sin backend
+  switchLights (): void {
+    if (this.isLightsSame()){
+      for (let light = 0; light < 5; light++) {
+        this.lightsOn[light] = !this.lightsOn[light]; 
+      }
+    }
+    else{
+      for (let light = 0; light < 5; light++) {
+        this.lightsOn[light] = false; 
+      }
+    }
+  }
+
+  isLightsSame (): boolean {
+    let same = this.lightsOn[0];
+    for (let light = 1; light < 4; light++) {
+      if (same != this.lightsOn[light]){
+        return false;
+      }
+    }
+    return true;
+  }
 }
