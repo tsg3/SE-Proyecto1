cd37ad1d5f163a964f2f4ac93382ae92f34a65f9
Esteban Campos Granados <estebandcg1999@gmail.com>
2021-04-01 03:14:19 -0600

    Change of type of token storage from SessionStorage to LocalStorage. Now is possible to keep the session open and secure in multiple tabs.

--- embedded-system-app/src/app/_services/token-storage.service.ts
+++ embedded-system-app/src/app/_services/token-storage.service.ts
@@ -10,17 +10,17 @@ export class TokenStorageService {
   constructor() { }
 
   signOut(): void {
-    window.sessionStorage.clear();
+    window.localStorage.clear();
   }
 
   public saveToken(token: string): void {
-    window.sessionStorage.removeItem(TOKEN_KEY);
-    window.sessionStorage.setItem(TOKEN_KEY, token);
+    window.localStorage.removeItem(TOKEN_KEY);
+    window.localStorage.setItem(TOKEN_KEY, token);
   }
 
   public getToken(): string {
     // return sessionStorage.getItem(TOKEN_KEY);
-    let token = sessionStorage.getItem(TOKEN_KEY);
+    let token = localStorage.getItem(TOKEN_KEY);
     if (token == null)
       token = '';
     return token;
